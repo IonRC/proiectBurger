63acc55cfa29d5c76c3dd61e95f6bff51dd8548e
AlexandruGru <agrumeza1@gmail.com>
2021-11-19 00:29:10 +0200

    a little bit of change in html, added event listeners for tob bar and bar in left, added script to create list in left

--- script.js
+++ script.js
@@ -1,61 +1,108 @@
-const groups = document.querySelectorAll('.ingredientsGroups');
+const groupsOfIngredients = document.querySelectorAll('.variation-list');
 let leftBar = document.querySelector('.left');
-const arrForBread = ['imgs/burgher_white.png', '10 lei', 'imgs/burgher_white.png', '20 lei'];
-const arrForMeat = ['imgs/burger_meat.png', '10 lei', 'imgs/burger_meat.png', '20 lei', 'imgs/burger_meat.png', '10 lei', 'imgs/burger_meat.png', '30 lei'];
-const arrForVegetables = ['imgs/tomato_slice.png', '10 lei', 'imgs/tomato_slice.png', '20 lei'];
-const arrForCheese = ['imgs/cheese_slice.png', '10 lei', 'imgs/cheese_slice.png', '20 lei', 'imgs/cheese_slice.png', '10 lei', 'imgs/cheese_slice.png', '30 lei'];
-const arrForSauce = ['imgs/sauce.png', '10 lei', 'imgs/sauce.png', '30 lei']
 
-let finalPrice;
+const objForBread = [{img: 'imgs/burgher_white.png', img1: 'imgs/breadWhiteTop.png', img2: 'imgs/breadWhiteBot.png', name: 'paine1', price: 10}, {img: 'imgs/burgher_white.png', name: 'paine2', price: 20}];
+const objForMeat = [{img: 'imgs/burger_meat.png', name: 'parjoala1', price: 20}, {img: 'imgs/burger_meat.png', name: 'parjoala2', price: 30}, {img: 'imgs/burger_meat.png', name: 'parjoala3', price: 40}];
+const objForVegetables = [{img: 'imgs/tomato_slice.png', name: 'legume1', price: 10}, {img: 'imgs/tomato_slice.png', name: 'legume2', price: 20}];
+const objForCheese = [{img: 'imgs/cheese_slice.png', name: 'cascaval1', price: 20}, {img: 'imgs/cheese_slice.png', name: 'cascaval2', price: 30}, {img: 'imgs/cheese_slice.png', name: 'cascaval3', price: 40}];
+const objForSauce = [{img: 'imgs/sauce.png', name: 'sous1', price: 10}, {img: 'imgs/sauce.png', name: 'sous2', price: 20}];
 
-groups.forEach(elem => {
+groupsOfIngredients.forEach(elem => {
   elem.addEventListener('click', (ev) => {
+    // bread
     if(ev.target.parentElement.className === 'liBread') {
-      insertGroup(arrForBread);
-      console.log(`bread`);
-      // functia pentru a inserta in div.left painea si a inserta in ul din right pozitia adugata si a adauga la pret final
+      insertLeftGroup(objForBread);
+      let pickIngridientGroup = document.querySelectorAll('.left > ul > li');
+      pickIngridientGroup.forEach(elem => {
+        elem.addEventListener('click', (ev) => {
+          objForBread.forEach(elem =>{
+            if(elem.name === ev.currentTarget.classList.value) {
+            //  aici va merge codul pentru a crea contentul in dependenta care element din lista a fost ales
+            }
+          })
+        })
+      })
     };
+    // meat
     if(ev.target.parentElement.className === 'liMeat') {
-      insertGroup(arrForMeat);
-      console.log(`meat`);
-      // functia pentru a inserta in div.left pirjoala si a inserta in ul din right pozitia adugata si a adauga la pret final
+      insertLeftGroup(objForMeat);
+      let pickIngridientGroup = document.querySelectorAll('.left > ul > li');
+      pickIngridientGroup.forEach(elem => {
+        elem.addEventListener('click', (ev) => {
+          objForMeat.forEach(elem =>{
+            if(elem.name === ev.currentTarget.classList.value) {
+              //  aici va merge codul pentru a crea contentul in dependenta care element din lista a fost ales
+            }
+          })
+        })
+      })
     };
+    // vegetables
     if(ev.target.parentElement.className === 'liVegetables') {
-      insertGroup(arrForVegetables);
-      console.log(`vegetables`);
-      // functia pentru a inserta in div.left legumele si a inserta in ul din right pozitia adugata si a adauga la pret final
+      insertLeftGroup(objForVegetables);
+      let pickIngridientGroup = document.querySelectorAll('.left > ul > li');
+      pickIngridientGroup.forEach(elem => {
+        elem.addEventListener('click', (ev) => {
+          objForVegetables.forEach(elem =>{
+            if(elem.name === ev.currentTarget.classList.value) {
+            //  aici va merge codul pentru a crea contentul in dependenta care element din lista a fost ales              
+            }
+          })
+        })
+      })
     };
+    // cheese
     if(ev.target.parentElement.className === 'liCheese') {
-      insertGroup(arrForCheese);
-      console.log(`cheese`);
-      // functia pentru a inserta in div.left cascavalul si a inserta in ul din right pozitia adugata si a adauga la pret final
+      insertLeftGroup(objForCheese);
+      let pickIngridientGroup = document.querySelectorAll('.left > ul > li');
+      pickIngridientGroup.forEach(elem => {
+        elem.addEventListener('click', (ev) => {
+          objForCheese.forEach(elem =>{
+            if(elem.name === ev.currentTarget.classList.value) {
+            //  aici va merge codul pentru a crea contentul in dependenta care element din lista a fost ales
+            }
+          })
+        })
+      })
     };
+    // sauce
     if(ev.target.parentElement.className === 'liSauce') {
-      insertGroup(arrForSauce);
-      console.log(`sauce`);
-      // functia pentru a inserta in div.left sousul si a inserta in ul din right pozitia adugata si a adauga la pret final
+      insertLeftGroup(objForSauce);
+      let pickIngridientGroup = document.querySelectorAll('.left > ul > li');
+      pickIngridientGroup.forEach(elem => {
+        elem.addEventListener('click', (ev) => {
+          objForSauce.forEach(elem =>{
+            if(elem.name === ev.currentTarget.classList.value) {
+            //  aici va merge codul pentru a crea contentul in dependenta care element din lista a fost ales  
+            }
+          })
+        })
+      })
     };
   })
 })
 
-let insertGroup = (arr) => {
-  let breadUl = document.createElement('ul');
-  leftBar.innerHTML = '';
-  for (let i = 0; i < arr.length; i += 2) {
+let insertLeftGroup = (intel) => {
+  let leftList = document.querySelector('.ingredients-list')
+  leftList.innerHTML = '';
+  for (let i = 0; i < intel.length; i ++) {
     let li = document.createElement('li');
-    let imgLi = document.createElement('img');
-    imgLi.setAttribute('src', arr[i]);
-    li.appendChild(imgLi);
-    let textLi = document.createElement('span') 
-    textLi.textContent = arr[i+1];
-    li.appendChild(textLi);
-    breadUl.appendChild(li);
-    let buttonLi = document.createElement('button');
-    let imgButton = document.createElement('img');
-    imgButton.setAttribute('src', 'imgs/green_plus.png');
-    buttonLi.appendChild(imgButton);
-    li.appendChild(buttonLi);
+      li.classList.add(intel[i].name)
+      let imgLi = document.createElement('img');
+      imgLi.setAttribute('src', intel[i].img);
+      li.appendChild(imgLi);
+        
+      let textLi = document.createElement('span') 
+      textLi.textContent = `${intel[i].name}; ${intel[i].price} lei`;
+      li.appendChild(textLi);
+      leftList.appendChild(li);
+
+      let buttonLi = document.createElement('button');
+      let imgButton = document.createElement('img');
+      imgButton.setAttribute('src', 'imgs/green_plus.png');
+      buttonLi.appendChild(imgButton);
+      li.appendChild(buttonLi);
   };
-  leftBar.appendChild(breadUl)
+  leftBar.appendChild(leftList);
 }
 
